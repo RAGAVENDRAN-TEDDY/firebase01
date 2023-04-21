140d726430f8e54bb413fc655cd8f321a5305634
ragavendran-teddy <hariragav1998@gmail.com>
2023-04-21 16:15:14 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { environment } from '../environments/environment';
+// import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
